d86b6dbcd1401cea382ad04fe496e542a47706ca
Andei Salanoi <a.salanoi@godeltech.com>
2018-03-12 16:29:36 +0300

    Property added

--- TeamCityDemo.CommandLineTool.Tests/Class1.cs
+++ TeamCityDemo.CommandLineTool.Tests/Class1.cs
@@ -10,6 +10,12 @@ namespace TeamCityDemo.CommandLineTool.Tests
     [TestFixture]
     public class DisposableTests
     {
+        public string _field;
+
+        string field2;
+
+        public string property1 { get; set; }
+
         [Test]
         public void DoTest()
         {
